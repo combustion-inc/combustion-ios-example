d3baca0d1a25a397f66295cccb6efed834fea585
jmaha <jmachacek@gmail.com>
2021-12-10 08:55:55 -0800

    Feature/initial setup (#1)
    * Added engineering/example app as separate repository.
    
    * Disabled 'share' button if logs aren't up to date.
    
    * Added BLE background mode and some README notes.
    
    * Converted to use new Probe class instead of Device.
    
    * Added Bluetooth plist items.
    
    * Fixed Probed Row and Probe details to observe the probe
    
    Co-authored-by: Jesse Johnston <jesse.johnston@gmail.com>

---
+++ Combustion iOS Example/Combustion iOS Example/EngineeringScreens/EngineeringProbeRow.swift
@@ -0,0 +1,46 @@
+//  EngineeringProbeRow.swift
+
+/*--
+MIT License
+
+Copyright (c) 2021 Combustion Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+--*/
+
+import SwiftUI
+import CombustionBLE
+
+struct EngineeringProbeRow: View {
+    @ObservedObject var probe: Probe
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Serial = \(probe.name)")
+            Text("MAC = \(probe.macAddressString)")
+            Text("RSSI   = \(probe.rssi)")
+            
+            if let temps = probe.currentTemperatures {
+                let tempStrings = temps.values.map { String(format: "%.02f", $0) }
+                Text("\(tempStrings[0]), \(tempStrings[1]), \(tempStrings[2]), \(tempStrings[3])")
+                Text("\(tempStrings[4]), \(tempStrings[5]), \(tempStrings[6]), \(tempStrings[7])")
+            }
+        }
+    }
+}
